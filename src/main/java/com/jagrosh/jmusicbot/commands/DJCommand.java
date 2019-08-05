6e8340743841d21b772b8aaffa7bff944def7300
Seyyid Ahmed Doğan <contact@seyahdoo.com>
2019-08-05 13:52:44 +0300

    Bypassed DJ role check

--- src/main/java/com/jagrosh/jmusicbot/commands/DJCommand.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/DJCommand.java
@@ -29,17 +29,9 @@ public abstract class DJCommand extends MusicCommand
     public DJCommand(Bot bot)
     {
         super(bot);
-        this.category = new Category("DJ", event -> 
+        this.category = new Category("DJ", event ->
         {
-            if(event.getAuthor().getId().equals(event.getClient().getOwnerId()))
-                return true;
-            if(event.getGuild()==null)
-                return true;
-            if(event.getMember().hasPermission(Permission.MANAGE_SERVER))
-                return true;
-            Settings settings = event.getClient().getSettingsFor(event.getGuild());
-            Role dj = settings.getRole(event.getGuild());
-            return dj!=null && (event.getMember().getRoles().contains(dj) || dj.getIdLong()==event.getGuild().getIdLong());
+            return true;
         });
     }
 }
